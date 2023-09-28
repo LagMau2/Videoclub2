c94a0f58f897758f55a3f0a7adc06c5832b57622
Mau Ramirez <a348705@uach.mx>
2023-09-28 08:58:05 -0600

    Se agrega director

---
+++ db.js
@@ -0,0 +1,11 @@
+const sequelize = require("sequelize");
+
+const directorModel = require('./models/director');
+
+/*
+Para conectar con la base de datos se necesita:
+1- Nombre de la base de datos
+2- Usuario
+3- Contraseña
+4- Objeto de configuracion ORM
+*/
